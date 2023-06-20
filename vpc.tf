a27f129279f94c819b87ed4a06325c4e86f2c213
zenoweed <43124953+zenoweed@users.noreply.github.com>
2023-06-20 18:23:54 +0530

    Update vpc.tf
    Added route table + association with public and private Subnets

--- vpc.tf
+++ vpc.tf
@@ -17,3 +17,29 @@ resource "aws_internet_gateway" "IGW" {    # Creating Internet Gateway
    vpc_id =  aws_vpc.Main.id
    cidr_block = "${var.private_subnets}"          # CIDR block of private subnets
  }
+
+ resource "aws_route_table" "PublicRT" {    # Creating RT for Public Subnet
+    vpc_id =  aws_vpc.Main.id
+         route {
+    cidr_block = "0.0.0.0/0"               # Traffic from Public Subnet reaches Internet via Internet Gateway
+    gateway_id = aws_internet_gateway.IGW.id
+     }
+ }
+
+ resource "aws_route_table" "PrivateRT" {    # Creating RT for Private Subnet
+   vpc_id = aws_vpc.Main.id
+   route {
+   cidr_block = "0.0.0.0/0"             # Traffic from Private Subnet reaches Internet via NAT Gateway
+   nat_gateway_id = aws_nat_gateway.NATgw.id
+   }
+ }
+
+ resource "aws_route_table_association" "PublicRTassociation" {
+    subnet_id = aws_subnet.publicsubnets.id
+    route_table_id = aws_route_table.PublicRT.id
+ }
+ 
+ resource "aws_route_table_association" "PrivateRTassociation" {
+    subnet_id = aws_subnet.privatesubnets.id
+    route_table_id = aws_route_table.PrivateRT.id
+ }
